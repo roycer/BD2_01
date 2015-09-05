27c403b0649ece4722cad5251e4959daa7e8c006
roycer <roycercordova@gmail.com>
2015-09-05 16:01:17 -0500

    garbage item
    eliminando elementos que no son parte del proyecto

--- script/main.js
+++ script/main.js
@@ -1,40 +1,126 @@
 var app = angular.module('myApp', ['includeExample','checklist-model','ui.filters']);
 
-app.controller('myCtrl',  function($scope, $http) {
- 
-  
+app.service('service_Data',function($http){
+    var ordenes = [];
+    var productos=[];
+    var sold=[];
+    var categories=[];
+    var facturas=[];
+    var clientes=[];
 
+    this.hhtpOrden = function (){
+        return $http.get("server/orden.php");
+    };
+    this.hhtpProduct = function (){
+        return $http.get("server/producto.php?product");
+    };
+    this.hhtpSold = function (){
+        return $http.get("server/producto.php?sold");
+    };
+    this.hhtpCategory = function (){
+        return $http.get("server/producto.php?category");
+    };
+    this.hhtpFactura = function (){
+        return $http.get("server/factura.php");
+    };
+    this.hhtpCliente = function (){
+        return $http.get("server/cliente.php");
+    };
+
+    this.setOrden= function(new_ordenes){ ordenes = new_ordenes;};
+    this.setProduct = function(new_productos){productos = new_productos;}
+    this.setSold = function(new_sold){sold = new_sold;}
+    this.setCategory = function(new_categories){categories = new_categories;}
+    this.setFactura = function(new_facturas){facturas = new_facturas;}
+    this.setCliente = function(new_clientes){clientes = new_clientes;}
+
+    this.getOrden = function(){ return ordenes;};
+    this.getProduct = function(){ return productos;};
+    this.getSold = function(){ return sold;};
+    this.getCategory = function(){ return categories;};
+    this.getFactura = function(){ return facturas;};
+    this.getCliente = function(){ return clientes;};
 });
 
-app.controller('myCtrlOrden',  function($scope, $http) {
-     $http.get("server/orden.php").success(function (response) {$scope.ordenes = response;});
-     $scope.ordenarPor = function(orden) { $scope.ordenSeleccionado = orden; };
+app.controller('myCtrl',  function($scope, service_Data) {
+    service_Data.hhtpOrden().success(function (response) { 
+        service_Data.setOrden(response); 
+        $scope.ordenes = service_Data.getOrden();
+    });
+    service_Data.hhtpProduct().success(function(response){ 
+        service_Data.setProduct(response);
+        $scope.productos = service_Data.getProduct();
+    });
+    service_Data.hhtpSold().success(function (response) { 
+        service_Data.setSold(response);
+        $scope.products_sold = service_Data.getSold();
+    });
+    service_Data.hhtpCategory().success(function (response) { 
+        service_Data.setCategory(response);
+        $scope.product_categories = service_Data.getCategory();
+    });
+    service_Data.hhtpFactura().success(function(response){ 
+        service_Data.setFactura(response);
+        $scope.invoices = service_Data.getFactura();
+    });
+    service_Data.hhtpCliente().success(function(response){
+        service_Data.setCliente(response);
+        $scope.clientes = service_Data.getCliente();
+    });
+    $scope.ordenarPor = function(orden) { $scope.ordenSeleccionado = orden; };
 });
 
-app.controller('myCtrlProducto',  function($scope, $http) {
-    $http.get("server/producto.php?product").success(function(response){ $scope.productos = response;})
-    $http.get("server/producto.php?sold").success(function (response) { $scope.products_sold = response;});
-    $http.get("server/producto.php?category").success(function (response) { $scope.product_categories = response;});
+app.controller('myCtrlOrden',  function($scope, $http, service_Data) {
+
+    $scope.List = {};
+    $scope.newOrderItems = {};
+    $scope.newOrden = {};
+
+    $scope.setTotals = function(){
+        var costoTotal = 0;
+        for (i = 0; i < $scope.List['productos_seleccionados'].length; i++) { 
+          costoTotal += $scope.newOrderItems['order_item_cost'][$scope.List['productos_seleccionados'][i]['product_id']];
+        }
+        $scope.newOrden.total_amount = costoTotal;
+    }
+
+    $scope.add = function(){
+
+        $http({
+          method: 'GET',
+          url: 'server/orden.php',
+          params:{ newOrden: JSON.stringify($scope.newOrden) , newOrderItems: JSON.stringify($scope.newOrderItems) , List: JSON.stringify($scope.List)}  
+        });
+        service_Data.hhtpOrden().success(function (response) { 
+        service_Data.setOrden(response); 
+        $scope.ordenes = service_Data.getOrden();
+    });
+
+    };
+});
+
+app.controller('myCtrlProducto',  function($scope, service_Data) {
+    
+    
     $scope.productSelect=null;
 
-    $scope.ordenarPor = function(orden) { $scope.ordenSeleccionado = orden; };
+    
     $scope.detalles = function(objeto) { $scope.productSelect = objeto; };
 });
 
-app.controller('myCtrlFactura',  function($scope, $http) {
-    $http.get("server/factura.php").success(function(response){ $scope.invoices = response;})
+app.controller('myCtrlFactura',  function($scope, service_Data) {
+    
     $scope.invoiceSelect = null;
 
-    $scope.ordenarPor = function(orden) { $scope.ordenSeleccionado = orden; };
+  
     $scope.detalles = function(objeto) { $scope.invoiceSelect = objeto; };
 });
 
-app.controller('myCtrlCliente',  function($scope, $http) {
+app.controller('myCtrlCliente',  function($scope, service_Data) {
 
-    $http.get("server/cliente.php").success(function(response){$scope.clientes = response;});
     $scope.generos = [{name:'Masculino'},{name:'Femenino'}];
-     $scope.customerSelect = null;
-    $scope.ordenarPor = function(orden) { $scope.ordenSeleccionado = orden; };
+    $scope.customerSelect = null;
+
     $scope.detalles = function(objeto) { $scope.customerSelect = objeto; };
 });
 
--- server/orden.php
+++ server/orden.php
@@ -9,4 +9,42 @@ function getOrden(){
 	return getJSON($sql);
 }
 
+
+if(isset($_GET['newOrden'], $_GET['newOrderItems'], $_GET['List'] )){
+
+	$newOrden = json_decode($_GET['newOrden']);
+	$newOrderItems = json_decode($_GET['newOrderItems']);
+	$List = json_decode($_GET['List']);
+	//$new_date = date('Y-m-d H:i:s', strtotime($HoraFin));
+
+	$order_id  = $newOrden->{'order_id'};
+	$customer_id = $newOrden->{'customer_id'};
+	$total_amount = $newOrden->{'total_amount'};
+
+	insertar_newOrden($order_id,$customer_id,$total_amount);
+	$contador = 0;
+	foreach ($List->{'productos_seleccionados'} as $objProductSelect){
+		$product_id =  $objProductSelect->{'product_id'};
+	    $product_quantity = $newOrderItems->{'product_quantity'}->{$product_id};
+	    $order_item_cost = $newOrderItems->{'order_item_cost'}->{$product_id};
+	    $contador = $contador + 1;
+	    $order_item_id = $order_id . rand(0,9) . $contador;
+	    insertar_newOrderItems($order_item_id, $order_id, $product_id, $product_quantity, $order_item_cost);
+	}
+}
+
+function insertar_newOrden($order_id,$customer_id,$total_amount){
+    $pdo = conectar();
+    $sql = "INSERT INTO orders (order_id, customer_id, date_order_placed, total_amount, est_registro) VALUES (:order_id, :customer_id, CURDATE(), :total_amount, 1)";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute(array(':order_id'=>$order_id, ':customer_id'=>$customer_id, ':total_amount'=>$total_amount));
+}
+
+function insertar_newOrderItems($order_item_id, $order_id, $product_id, $product_quantity, $order_item_cost){
+    $pdo = conectar();
+    $sql = "INSERT INTO order_items (order_item_id, order_id, product_id, product_quantity, order_item_cost) VALUES(:order_item_id, :order_id, :product_id,:product_quantity, :order_item_cost)";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute(array(':order_item_id'=>$order_item_id,':order_id'=>$order_id,':product_id'=>$product_id,':product_quantity'=>$product_quantity,':order_item_cost'=>$order_item_cost));
+}
+
 ?>
